99477d90136c8099bb4e13bd76ae7bdaf1cbe097
Yosip567 <49847086+Yosip567@users.noreply.github.com>
2021-12-02 00:05:57 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,4 +61,19 @@
 					visibleClass: 'navPanel-visible'
 				});
 
+		// Carusel
+			$('.owl-1').owlCarousel({
+
+	          loop:true,
+	          margin:0,
+	          nav:true,
+	          items: 1,
+	          smartSpeed: 1000,
+	          autoplay: false,
+	          autoplayHoverPause: true,
+	          navText: ['<span class="icon-keyboard_arrow_left">', '<span class="icon-keyboard_arrow_right">']
+	        });
+
 })(jQuery);
+
+
